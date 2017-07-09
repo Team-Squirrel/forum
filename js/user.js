0c47052c38a20e63a8bde53f73eb8928fb60519c
startere <ivanovemilplamenov@gmail.com>
2017-07-09 09:52:31 +0300

    updated user.js

--- js/user.js
+++ js/user.js
@@ -2,7 +2,7 @@ import * as utils from "utils";
 
 class User {
 
-    constructor(name, email, country) {
+    constructor(name, email) {
         this.name = name;
         this.email = email;
         this.role = "user";
@@ -10,4 +10,40 @@ class User {
         this.id = utils.userIdGenerator.next();
     }
 
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        if(!name){
+            throw "Username cannot be null.";
+        }
+
+        if(name.length === 0){
+            throw "Username cannot be empty.";
+        }
+        
+        this._name = name;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(e_mail){
+
+        if(!e_mail){
+            throw "Email cannot be null.";
+        }
+
+        if(e_mail.indexOf('@') === -1){
+            throw "Invalid email address.";
+        }
+
+        this._email = e_mail;
+    }
 }
+
+export {
+    User
+};
